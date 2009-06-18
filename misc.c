5dfd27651ea8689965254c5021747dea1020e780
jdp <jdp34@njit.edu>
2009-06-17 20:33:02 -0400

    Moved some functions into macros

--- misc.c
+++ misc.c
@@ -1,10 +1,5 @@
 #include "psionrl.h"
 
-/* Wait for and return a keypress */
-TCOD_key_t getkey(void) {
-	return(TCOD_console_wait_for_keypress(1));
-}
-
 /* Forces player to pick a key within a list */
 char choice(char *options) {
 	TCOD_key_t k;
@@ -18,23 +13,8 @@ char choice(char *options) {
 	return(k.c);
 }
 
-/* Change the current foreground color */
-void fgcolor(TCOD_color_t c) {
-	TCOD_console_set_foreground_color(NULL, c);
-}
-
-/* Change the current background color */
-void bgcolor(TCOD_color_t c) {
-	TCOD_console_set_background_color(NULL, c);
-}
-
-/* A generic string write function */
-void putstr(int x, int y, char *str) {
-	TCOD_console_print_left(NULL, x, y, TCOD_BKGND_SET, str);
-}
-
 /* A generic variable-length string write function */
-void putstrf(int x, int y, char *fmt, ...) {
+void putstrf(TCOD_console_t *con, int x, int y, char *fmt, ...) {
 	va_list vl;
 	char buffer[80];
 	
@@ -42,20 +22,6 @@ void putstrf(int x, int y, char *fmt, ...) {
 	vsprintf(buffer, fmt, vl);
 	va_end(vl);
 	
-	putstr(x, y, buffer);
+	putstr(con, x, y, buffer);
 }
 
-/* Place a character on the screen */
-void putch(int x, int y, unsigned char c) {
-	TCOD_console_put_char(NULL, x, y, c, TCOD_BKGND_SET);
-}
-
-/* Clear the screen */
-void clear(void) {
-	TCOD_console_clear(NULL);
-}
-
-/* Update the screen */
-void update(void) {
-	TCOD_console_flush();
-}
